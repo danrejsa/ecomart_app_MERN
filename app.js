88647cb241e17d5448ca545fada58931a4f575b3
Daniel <christopher.daniel@aun.edu.ng>
2019-08-04 01:00:34 +0100

    ecomartV1

--- app.js
+++ app.js
@@ -28,12 +28,14 @@ app.use('/api/reports', reports)
 app.use('/api/auth', auth)
 app.use('/api/cars', cars);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client' ,'build', 'index.html'))
-    })
-  }
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
   
 module.exports = app;
 
